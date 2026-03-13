235626e594790d189517e011218e46f8469a752b
Yelyzaveta <yst220@aubg.edu>
2026-03-13 18:29:21 +0200

    moved the majority of the application styling to the styles.css file, added changes to app.routes.ts and app.config.ts

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,40 +1,21 @@
-// import { Routes } from '@angular/router';
-// import { LoginComponent } from './components/login/login';
-// import { RegisterComponent } from './components/register/register';
-// import{DashboardComponent} from './components/dashboard/dashboard';
-// import { LayoutComponent } from './components/layout/layout';
-// import { AuthGuard } from './guards/auth.guard';
-// export const routes: Routes = [
-//   { path: '', redirectTo: '/login', pathMatch: 'full' },
-//   { path: 'login', component: LoginComponent },
-//   { path: 'register', component: RegisterComponent },
- 
- 
-
-//   { path: '', 
-//   component: LayoutComponent,
-// canActivate: [AuthGuard],
-//   children:[{path: 'dashboard', component: DashboardComponent}] },
-//   { path: '**', redirectTo: '/login' }
-// ];
 import { Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login';
 import { RegisterComponent } from './components/register/register';
-import { DashboardComponent } from './components/dashboard/dashboard';
-import { AuthGuard } from './guards/auth.guard';
 import { LayoutComponent } from './components/layout/layout';
-import { roleguard } from './guards/roleguard';
-import { TripsComponent } from './components/trips/trips';
-import { ShipmentsComponent } from './components/shipments/shipments';
-import { OrdersComponent } from './components/orders/orders';
+import { AuthGuard } from './guards/auth.guard';
+import { RoleGuard } from './guards/roleguard';
 import { DashboardCustomerComponent } from './components/dashboardcustomer/dashboardcustomer';
 import { DashboardDispatcherComponent } from './components/dashboarddispatcher/dashboarddispatcher';
 import { DashboardDriverComponent } from './components/dashboarddriver/dashboarddriver';
+import { OrdersComponent } from './components/orders/orders';
+import { PendingOrdersComponent } from './components/pendingorders/pendingorders';
+import { ShipmentsComponent } from './components/shipments/shipments';
+import { TripsComponent } from './components/trips/trips';
 import { DriverAvailabilityComponent } from './components/driveravailability/driveravailability';
 
 export const routes: Routes = [
 
-  //  PUBLIC ROUTES (NO LAYOUT)
+  // Public routes 
   {
     path: '',
     children: [
@@ -43,68 +24,60 @@ export const routes: Routes = [
     ]
   },
 
-  //  PROTECTED ROUTES (WITH LAYOUT)
+  // Protected routes 
   {
     path: '',
     component: LayoutComponent,
     canActivate: [AuthGuard],
     children: [
-
-      {
-        path: 'dashboard',
-        component: DashboardComponent,
-        canActivate: [roleguard],
-        data: { roles: ['Admin'] }
-      },
-
-      {
-        path: 'trips',
-        component: TripsComponent,
-        canActivate: [roleguard],
-        data: { roles: ['Driver', 'Dispatcher', 'Admin'] }
-      },
-
-      {
-        path: 'shipments',
-        component: ShipmentsComponent,
-        canActivate: [roleguard],
-        data: { roles: ['Dispatcher', 'Admin'] }
-      },
-
-      {
-        path: 'orders',
-        component: OrdersComponent,
-        canActivate: [roleguard],
-        data: { roles: ['Customer'] }
-      },
-
       {
         path: 'dashboardcustomer',
         component: DashboardCustomerComponent,
-        canActivate: [roleguard],
+        canActivate: [RoleGuard],
         data: { roles: ['Customer'] }
       },
       {
         path: 'dashboarddispatcher',
         component: DashboardDispatcherComponent,
-        canActivate: [roleguard],
+        canActivate: [RoleGuard],
         data: { roles: ['Dispatcher'] }
       },
-
       {
         path: 'dashboarddriver',
         component: DashboardDriverComponent,
-        canActivate: [roleguard],
+        canActivate: [RoleGuard],
         data: { roles: ['Driver'] }
       },
-
-      {path:'driveravailability',
-      component: DriverAvailabilityComponent  ,
-      canActivate: [roleguard],
-      data: { roles: ['Driver'] }
+      {
+        path: 'orders',
+        component: OrdersComponent,
+        canActivate: [RoleGuard],
+        data: { roles: ['Customer'] }
+      },
+      {
+        path: 'pendingorders',
+        component: PendingOrdersComponent,
+        canActivate: [RoleGuard],
+        data: { roles: ['Dispatcher'] }
+      },
+      {
+        path: 'shipments',
+        component: ShipmentsComponent,
+        canActivate: [RoleGuard],
+        data: { roles: ['Dispatcher', 'Admin'] }
+      },
+      {
+        path: 'trips',
+        component: TripsComponent,
+        canActivate: [RoleGuard],
+        data: { roles: ['Driver', 'Dispatcher', 'Admin'] }
+      },
+      {
+        path: 'driveravailability',
+        component: DriverAvailabilityComponent,
+        canActivate: [RoleGuard],
+        data: { roles: ['Driver'] }
       }
-    
-
     ]
   },
 
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,15 +1,13 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
-import { provideRouter } from '@angular/router'; 
-import { routes } from './app/app.routes'; 
 import { authInterceptor } from './app/interceptors/auth.interceptor';
 import { provideHttpClient, withInterceptors  } from '@angular/common/http';
+
 bootstrapApplication(App, {
   ...appConfig,
   providers: [
     ...appConfig.providers,
-    provideRouter(routes),
     provideHttpClient(withInterceptors([authInterceptor]))
   ]
 })
